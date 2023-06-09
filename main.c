f04e3f91b6f616cd4ad136e795535af4c6f0698f
CarnisJr <mirandab@gmail.com>
2023-06-08 21:04:27 -0500

    main.c: varibales, constantes y función 'interfaz' agregadas

--- main.c
+++ main.c
@@ -1,10 +1,21 @@
 #include<stdlib.h>
 #include<stdio.h>
+#include<ctype.h>
 #include "funciones.h"
+
+//Definir constantes
+#define MAX_DIAS 10
+#define MAX_SED 100
+#define MAX_HAMBRE 100
+#define MAX_SALUD 100
 
 int main(void){
 
-    rand();
-    system("pause");
+    //Definir variables
+    int sed, hambre, vida = 100;
+    char selector;
+
+    interfaz(selector, vida);
+
     return 0;
 }
